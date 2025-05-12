3a1b5d14dbb216f7abd89463cdf5fc43a15dac95
Shachar <shar535909@gmail.com>
2025-05-12 17:37:19 +0300

    Creation of function 'getArgsDeft'

--- Program.cs
+++ Program.cs
@@ -8,8 +8,19 @@ namespace Project_12_05_25
 {
     internal class Program
     {
+        static void CallArgs(string[] args)
+        {/*
+          * A func that receives arguments from the debuggr(defalt)
+          */
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+        
         static void Main(string[] args)
         {
+            CallArgs(args);
         }
     }
 }
